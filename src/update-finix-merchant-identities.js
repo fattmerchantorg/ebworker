23293eabd4b690b3f218860727bcd85741f1e102
Taylor Foster <taylorfoster@TF-ST-C02FQ0LLMD6X.local>
2021-11-23 16:05:30 -0500

    Working on logging for job.

--- src/update-finix-merchant-identities.js
+++ src/update-finix-merchant-identities.js
@@ -1,5 +1,8 @@
 const axios = require("axios");
 const { db } = require("../util/db-util");
+const csv = require("fast-csv");
+const { format } = require("@fast-csv/format");
+const fs = require("fs");
 
 const basicAuth = (finixAccessId, finixSecureKey) =>
   `Basic ${Buffer.from(`${finixAccessId}:${finixSecureKey}`).toString(
@@ -26,11 +29,15 @@ let paginateFrom = 0;
 let paginateTo = 500;
 
 const run = async () => {
+  const outputCsvStream = format({ headers: true });
+  const outputWriteStream = fs.createWriteStream("csv/output.csv");
+  outputCsvStream.pipe(outputWriteStream);
+
   // Fetch rows from fatt where there is a valid Finix Identity
   const rows = await new Promise((resolve, reject) => {
     db().query(
       `
-      select r.finix_identity_id, r.finix_merchant_id 
+      select m.id, r.finix_identity_id, r.finix_merchant_id 
       from ${fattDbName}.merchants as m 
       join ${fattDbName}.registrations as r
         on m.id = r.merchant_id
@@ -54,8 +61,28 @@ const run = async () => {
     console.log("no results");
     process.exit();
   }
-  
+
+
+  await Promise.all(rows.map(async (row, index) => {
+    outputCsvStream.write({
+      hello: index
+    });
+  }));
+
+  process.exit();
+
   for (const row of rows) {
+    outputCsvStream.write({
+      hello: 'world'
+    });
+    continue;
+    let o = {
+      PMID: row.id,
+      finix_identity_id: row.finix_identity_id,
+      finix_merchant_id: row.finix_merchant_id,
+      error_identity: null,
+      error_provision: null
+    };
     try {
       // Update the merchant identity in FINIX
       await axios({
@@ -69,8 +96,26 @@ const run = async () => {
         headers,
       });
       console.log(`Updated Transaction Limit For Indentity ID: ${row.finix_identity_id}`);
+      
     } catch(e) {
-      console.log('Error PUT request for /identities', e);
+      // o.error_identity = e && e.response && e.response.data && JSON.stringify(e.response.data);
+      // o.error_identity = o.error_identity ? o.error_identity : e;
+      console.log('Error PUT request for /identities/:ID', e);
+    }
+
+    try {
+      // Re-Provision Merchant
+      await axios({
+        url: `${process.env.FINIX_BASE_URL}/merchants/${row.finix_merchant_id}/verifications`,
+        method: "post",
+        data: {},
+        headers,
+      });
+      console.log(`Provisioned For Finix Merchant ID: ${row.finix_identity_id}`);
+    } catch(e) {
+      // o.error_provision = e && e.response && e.response.data && JSON.stringify(e.response.data);
+      // o.error_provision = o.error_provision ? o.error_provision : e;
+      console.log('Error POST request for /merchants/:ID/verifications', e);
     }
   }
 
